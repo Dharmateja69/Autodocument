fc18b11e13542f6add95cef9beb765af19d90d3e
Dharmateja69 <ddharmateja69@gmail.com>
2025-07-11 21:43:58 +0530

    testing for diffrent file types

--- src/testing/Test.js
+++ src/testing/Test.js
@@ -1,17 +1,43 @@
-let a = 20
-let b = 30
-let c = a + b
-console.log(`Sum of ${a} and ${b} is ${c}`);
+// def mergeSort(arr):
+//     # Base case: if the array has 0 or 1 element, it's already sorted
+// if len(arr) > 1:
+//     mid = len(arr) // 2  # Find the middle point to divide the array into two halves.
+// leftHalf = arr[:mid]  # Dividing the array elements into two halves.
+//     rightHalf = arr[mid:]  # Dividing the array elements into two halves.
 
-const sum = () => {
-    return a + b;
-}
-const multiply = () => {
-    return a * b;
-}
-const subtract = () => {
-    return a - b;
-}
-const divide = () => {
-    return a / b;
-}   
+//         mergeSort(leftHalf)  # Sort the first half recursively.
+//             mergeSort(rightHalf)  # Sort the second half recursively.
+
+//                 i = j = k = 0  # Initialize pointers for leftHalf, rightHalf, and the main array
+
+//         # Merge the two halves into the main array
+// while i < len(leftHalf) and j < len(rightHalf):
+// if leftHalf[i] < rightHalf[j]:
+//     arr[k] = leftHalf[i]
+// i += 1
+//             else:
+// arr[k] = rightHalf[j]
+// j += 1
+// k += 1
+
+//         # Copy any remaining elements from the left half
+// while i < len(leftHalf):
+//     arr[k] = leftHalf[i]
+// i += 1
+// k += 1
+
+//         # Copy any remaining elements from the right half
+// while j < len(rightHalf):
+//     arr[k] = rightHalf[j]
+// j += 1
+// k += 1
+
+// # Example usage:
+// if __name__ == '__main__':
+//     arr = [12, 11, 13, 5, 6, 7]
+// print("Given array is", arr)
+
+// mergeSort(arr)
+
+// print("Sorted array is: ", arr)
+
