bf05fcb8035e28376dd59b25e33660a38d1099cc
Dharmateja69 <ddharmateja69@gmail.com>
2025-07-13 12:10:00 +0530

    small change

--- src/testing/Test.js
+++ src/testing/Test.js
@@ -4,15 +4,6 @@ function test() {
     console.log("Test function executed");
 }
 
-let num = 1221
-let or = num
-let rev = 1
-while (num != 0) {
-    rev = rev * 10 + num % 10
-    num /= 10
-}
-if (rev == or) {
-    console.log(`it is a palindrome`)
-} else {
-    console.log(`it is not a palindrom`)
+for (let i = 0; i < 5; i++) {
+    console.log(`Iteration ${i}`);
 }
